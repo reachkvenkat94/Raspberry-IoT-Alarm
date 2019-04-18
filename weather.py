b48ba50125bbc3d5757551555a5d0fc34be54b26
krishvenkatprasad <krishvenkatprasad@users.noreply.github.com>
2019-04-18 14:51:19 -0400

    Create weather.py
    Display weather and time

---
+++ weather.py
@@ -0,0 +1,41 @@
+'''
+Created on Apr 16, 2019
+
+@author: Venkat Prasad Krishnamurthy
+'''
+from threading import Thread
+import pyowm
+import datetime
+from time import sleep 
+import serial
+
+
+class Display(Thread):
+    '''
+    classdocs
+    '''
+
+
+    def _init_(self):
+        '''
+        Constructor
+        '''
+        Thread._init_(self);
+        self.enableEmulator = True;
+        self.ser = serial.Serial('/dev/ttyACM0',9600)
+        
+    def run(self):
+        while self.enableEmulator == True:
+            owm = pyowm.OWM('270e9c1fe2c20c42dc7857efedc17320')
+            boston = owm.weather_at_place('Boston,US')
+            weather = boston.get_weather()
+            temp = weather.get_temperature('fahrenheit')['temp']
+            #print(temp)
+            now = datetime.datetime.now()
+            time = now.strftime("%H:%M")
+            stri = str(time)+"w"+str(temp);
+            #print(time)
+            seri = str.encode(stri)
+            self.ser.write(seri)
+            
+            sleep(5)
